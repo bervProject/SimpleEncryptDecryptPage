6be099382bdbddd69314a6be197399830ed00663
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-07-20 09:52:26 +0700

    Fix style

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,19 +19,3 @@
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      carousels: [
-        { text: "Slide 1", color: "primary" },
-        { text: "Slide 2", color: "info" },
-        { text: "Slide 3", color: "success" },
-        { text: "Slide 4", color: "warning" },
-        { text: "Slide 5", color: "danger" }
-      ]
-    };
-  }
-};
-</script>
--- src/views/Home.ts
+++ src/views/Home.ts
@@ -6,7 +6,7 @@ export default Vue.extend({
         { text: 'To keep your text secret, please use Embed.', color: 'info' },
         { text: 'To open your secret message, please use Extract', color: 'info' },
         { text: 'Enjoy this mini application', color: 'info' },
-      ]
+      ],
     };
-  }
+  },
 });

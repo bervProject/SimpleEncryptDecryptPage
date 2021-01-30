abb2f09889575adfc1578faf5a19e90c453c97cd
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-30 15:06:37 +0700

    Update json

--- src/views/Home.ts
+++ src/views/Home.ts
@@ -3,9 +3,18 @@ export default Vue.extend({
   data() {
     return {
       carousels: [
-        { text: 'To keep your text secret, please use Embed.', color: 'info', image: 'https://picsum.photos/seed/picsum/800/600?blur=2' },
-        { text: 'To open your secret message, please use Extract', color: 'info', image: 'https://picsum.photos/seed/picsum1/800/600?blur=2' },
-        { text: 'Enjoy this mini application', color: 'info', image: 'https://picsum.photos/seed/picsum5/800/600?blur=2' },
+        {
+          text: 'To keep your text secret, please use Embed.',
+          image: 'https://picsum.photos/seed/picsum/800/600?blur=2',
+        },
+        {
+          text: 'To open your secret message, please use Extract',
+          image: 'https://picsum.photos/seed/picsum1/800/600?blur=2',
+        },
+        {
+          text: 'Enjoy this mini application',
+          image: 'https://picsum.photos/seed/picsum5/800/600?blur=2',
+        },
       ],
     };
   },

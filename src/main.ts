4ef74616c0bee5f3951214e1b55409aabd0c859a
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-11-23 20:21:37 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { bulmaConfig } from '@oruga-ui/theme-bulma';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import '@oruga-ui/theme-bulma/dist/bulma.css';
+import '@oruga-ui/theme-bulma/style.css';
 
 const app = createApp(App);
 
